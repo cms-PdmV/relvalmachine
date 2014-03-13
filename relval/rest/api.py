c6192b7db5cbf6b550df3009b603beb492e34d1e
Zygimantas Gatelis <zygis.gg@gmail.com>
2014-03-13 12:11:18 +0100

    Refactored rest api

--- relval/rest/api.py
+++ relval/rest/api.py
@@ -54,66 +54,78 @@ class UsersListApi(Resource):
         self.users_dao.insertUser(**data)
 
 
-class PredefinedBlobsApi(Resource):
-    """ Predefined blobs resource
+class ListApi(object):
+    """ Base class for Resources that return paginated results
     """
 
     def __init__(self):
-        self.blobs_dao = PredefinedBlobsDao()
-
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('page_num', type=int)
         self.parser.add_argument('items_per_page', type=int)
         self.parser.add_argument('search', type=str)
 
-    @marshal_with(marshallers.blobs_marshaller_paginated)
-    def get(self):
-        """ Returns all existing predefined blobs
-        """
+    def get_result(self):
         args = self.parser.parse_args()
 
         if args['search']:
             page_num = args['page_num'] if args['page_num'] else 1
             items_per_page = args['items_per_page'] if args['items_per_page'] else None
-            blobs, total = self.__search(args['search'], page_num, items_per_page)
+            items, total = self.__search(args['search'], page_num, items_per_page)
         elif args['page_num'] and args['items_per_page']:
-            blobs, total = self.__get_page(args['page_num'], args['items_per_page'])
+            items, total = self.__get_page(args['page_num'], args['items_per_page'])
         else:
-            blobs, total = self.__get_page()
+            items, total = self.__get_page()
 
         resp = {
-            'items': blobs,
+            'items': items,
             'total': total
         }
         return resp
 
-    def post(self):
-        """ Creates new predefined blob
-        """
-        data = convert_keys_to_string(request.json)
-        self.blobs_dao.add(**data)
-
     def __search(self, query, page_num=1, items_per_page=None):
         return self.__paginated_result(
-            self.blobs_dao.search_all,
+            self.dao.search_all,
             page_num=page_num,
             items_per_page=items_per_page,
             query=query)
 
     def __get_page(self, page_num=1, items_per_page=None):
         return self.__paginated_result(
-            self.blobs_dao.get_paginated,
+            self.dao.get_paginated,
             page_num,
             items_per_page
         )
 
     def __paginated_result(self, dao_func, page_num=1, items_per_page=None, **kwargs):
         if not items_per_page:
-            items_per_page = app.config['BLOBS_PER_PAGE']
+            items_per_page = self.default_items_per_page
         result = dao_func(page_num=page_num, items_per_page=items_per_page, **kwargs)
         blobs = result.items
         total = result.total
         return blobs, total
+
+
+class PredefinedBlobsApi(Resource, ListApi):
+    """ Predefined blobs resource
+    """
+
+    def __init__(self):
+        ListApi.__init__(self)
+        super(ListApi, self).__init__()
+        self.dao = PredefinedBlobsDao()
+        self.default_items_per_page = app.config['BLOBS_PER_PAGE']
+
+    @marshal_with(marshallers.blobs_marshaller_paginated)
+    def get(self):
+        """ Returns all existing predefined blobs
+        """
+        return self.get_result()
+
+    def post(self):
+        """ Creates new predefined blob
+        """
+        data = convert_keys_to_string(request.json)
+        self.dao.add(**data)
 
 
 class PredefinedBlobApi(Resource):
@@ -143,67 +155,32 @@ class PredefinedBlobApi(Resource):
         self.blobs_dao.update(id=blob_id, **data)
 
 
-class StepsApi(Resource):
+class StepsApi(Resource, ListApi):
+    """ Steps resource
+    """
 
     def __init__(self):
-        self.steps_dao = StepsDao()
+        ListApi.__init__(self)
+        self.dao = StepsDao()
+        self.default_items_per_page = app.config['STEPS_PER_PAGE']
 
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('page_num', type=int)
-        self.parser.add_argument('items_per_page', type=int)
-        self.parser.add_argument('search', type=str)
 
     def post(self):
         """ Creates new step
         """
         data = convert_keys_to_string(request.json)
-        self.steps_dao.add(**data)
+        self.dao.add(**data)
 
     @marshal_with(marshallers.steps_marshaller_paginated)
     def get(self):
         """ Returns all existing steps
         """
-        args = self.parser.parse_args()
-
-        if args['search']:
-            page_num = args['page_num'] if args['page_num'] else 1
-            items_per_page = args['items_per_page'] if args['items_per_page'] else None
-            steps, total = self.__search(args['search'], page_num, items_per_page)
-        elif args['page_num'] and args['items_per_page']:
-            steps, total = self.__get_page(args['page_num'], args['items_per_page'])
-        else:
-            steps, total = self.__get_page()
-
-        resp = {
-            'items': steps,
-            'total': total
-        }
-        return resp
-
-    def __get_page(self, page_num=1, items_per_page=None):
-        return self.__paginated_result(
-            self.steps_dao.get_paginated,
-            page_num,
-            items_per_page
-        )
-
-    def __paginated_result(self, dao_func, page_num=1, items_per_page=None, **kwargs):
-        if not items_per_page:
-            items_per_page = app.config['STEPS_PER_PAGE']
-        result = dao_func(page_num=page_num, items_per_page=items_per_page, **kwargs)
-        steps = result.items
-        total = result.total
-        return steps, total
-
-    def __search(self, query, page_num=1, items_per_page=None):
-        return self.__paginated_result(
-            self.steps_dao.search_all,
-            page_num=page_num,
-            items_per_page=items_per_page,
-            query=query)
+        return self.get_result()
 
 
 class StepApi(Resource):
+    """ Step resource to work with single step
+    """
 
     def __init__(self):
         self.steps_dao = StepsDao()
