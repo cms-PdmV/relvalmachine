c7a278cb7ed75d107274f7d634f929dfaf00f4fb
Zygimantas Gatelis <zygis.gg@gmail.com>
2014-04-01 16:43:01 +0200

    Added loop in template

--- relval/services/commands_service.py
+++ relval/services/commands_service.py
@@ -4,7 +4,7 @@ from jinja2.loaders import FileSystemLoader, PackageLoader
 __author__ = "Zygimantas Gatelis"
 __email__ = "zygimantas.gatelis@cern.ch"
 
-from relval.database.dao import RequestsDao
+from relval.database.dao import RequestsDao, StepsDao
 from flask import render_template
 
 
@@ -13,14 +13,20 @@ class CommandsService(object):
     def __init__(self):
         self.env = Environment(loader=PackageLoader("relval", "/templates/bash"))
         self.request_dao = RequestsDao()
+        self.steps_dao = StepsDao()
 
     def get_test_command(self, request_id):
         template = self.env.get_template('test_request.sh')
+
         request = self.request_dao.get(request_id)
+        steps = []
+        for step in request.steps:
+            steps.append(self.steps_dao.get_details(step.id))
+            print steps[-1]
 
         return template.render(dict(
-            cmssw_release=request.cmssw_release)
-        )
+            cmssw_release=request.cmssw_release,
+            steps=steps))
 
 
 
--- relval/templates/bash/test_request.sh
+++ relval/templates/bash/test_request.sh
@@ -11,5 +11,11 @@ eval `scram runtime -sh`
 
 scram b
 cd ../../
+
+
+{% for step in steps %}
+{{step.text}} || exit $? ;
+{% endfor %}
+
 cmsDriver.py Configuration/GenProduction/python/B2G-Summer12-00461-fragment.py --filein "dbs:/Bprime_M-500_8TeV-madgraph/Summer12-START53_V7C-v1/GEN" --fileout file:B2G-Summer12-00461.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions START53_V7C::All --beamspot Realistic8TeVCollision --step GEN,SIM --python_filename B2G-Summer12-00461_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 0 || exit $? ;
 cmsRun -e -j B2G-Summer12-00461_rt.xml B2G-Summer12-00461_1_cfg.py || exit $? ;
