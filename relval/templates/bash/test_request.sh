c4c2bfb8ebb79637e3941eabdbef5f435b33639c
Zygimantas Gatelis <zygis.gg@gmail.com>
2014-04-07 10:30:02 +0200

    fixed test request script template

--- relval/templates/bash/test_request.sh
+++ relval/templates/bash/test_request.sh
@@ -17,6 +17,5 @@ cd ../../
 {{step.text}} -n {{step.events_num}} || exit $? ;
 {% endfor %}
 
-#cmsDriver.py Configuration/GenProduction/python/B2G-Summer12-00461-fragment.py --filein "dbs:/Bprime_M-500_8TeV-madgraph/Summer12-START53_V7C-v1/GEN" --fileout file:B2G-Summer12-00461.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions START53_V7C::All --beamspot Realistic8TeVCollision --step GEN,SIM --python_filename B2G-Summer12-00461_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 0 || exit $? ;
 
 cmsRun -e -j B2G-Summer12-00460_rt.xml B2G-Summer12-00460_1_cfg.py || exit $? ;
