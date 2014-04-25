f66befc7ac8df0945cc86fe561e839ac327d3941
Zygimantas Gatelis <zygis.gg@gmail.com>
2014-04-25 13:39:34 +0200

    Logs fetching made in same connection

--- relval/services/log_manager.py
+++ relval/services/log_manager.py
@@ -11,6 +11,21 @@ from relval import app
 from relval.services.ssh_service import SshService
 
 
+class LogFile(object):
+    """ Class to store data of a lig file.
+    """
+    def __init__(self, path, filename="", content=""):
+        self.path = path
+        self.filename = filename
+        self.content = content
+
+    def full_path(self):
+        if not self.filename:
+            return self.path
+        else:
+            return os.path.join(self.path, self.filename)
+
+
 class LogsManager(object):
     """ Service that manages logs from testing submission and other servers.
     """
@@ -28,30 +43,37 @@ class LogsManager(object):
         path = os.path.join(self.logs_dir, subdir)
         errors_file_name = self.__get_file_name(self.stderr_log(name))
         info_file_name = self.__get_file_name(self.stdout_log(name))
+        files_to_save = []
         if errors:
-            self.logs_handler.save_log_file(errors_file_name, path, errors)
+            files_to_save.append(LogFile(
+                path=path,
+                filename=errors_file_name,
+                content=errors
+            ))
         else:  # remove old log if no errors was
-            self.logs_handler.remove_log(os.path.join(path, errors_file_name))
+            self.logs_handler.remove_log(LogFile(path=os.path.join(path, errors_file_name)))
 
-        self.logs_handler.save_log_file(info_file_name, path, info)
+        files_to_save.append(LogFile(
+            path=path,
+            filename=info_file_name,
+            content=info
+        ))
+        self.logs_handler.save_log_file(files_to_save)
 
     def get_logs(self, name, subdir):
         """ Returns both stdout and stderr logs
         """
         path = os.path.join(self.logs_dir, subdir)
+
+        files = [
+            LogFile(path=path, filename=self.__get_file_name(self.stdout_log(name))),
+            LogFile(path=path, filename=self.__get_file_name(self.stderr_log(name)))
+        ]
+        contents = self.logs_handler.get_log_file(files)
+
         return "STDOUT:\n{0}\nSTDERR:\n{1}".format(
-            self.get_testing_std_out_log(name, path),
-            self.get_testing_std_error_log(name, path))
-
-    def get_testing_std_error_log(self, name, path):
-        file_name = self.__get_file_name(self.stderr_log(name))
-        path = os.path.join(path, file_name)
-        return self.logs_handler.get_log_file(path)
-
-    def get_testing_std_out_log(self, name, path):
-        file_name = self.__get_file_name(self.stdout_log(name))
-        path = os.path.join(path, file_name)
-        return self.logs_handler.get_log_file(path)
+            contents[0],
+            contents[1])
 
     def delete_old_test_log_files(self):
         self.logs_handler.delete_old_logs(app.config["DAYS_TO_KEEP_LOGS"], self.logs_dir)
@@ -81,13 +103,13 @@ class LogsManager(object):
 
 class LogsHandler(object):
 
-    def save_log_file(self, name, path, content):
+    def save_log_file(self, log_files):
         raise NotImplementedError("LogsHandler is abstract. Please extend and override this method.")
 
-    def get_log_file(self, path):
+    def get_log_file(self, log_files):
         raise NotImplementedError("LogsHandler is abstract. Please extend and override this method.")
 
-    def remove_log(self, path):
+    def remove_log(self, log_files):
         raise NotImplementedError("LogsHandler is abstract. Please extend and override this method.")
 
     def delete_old_logs(self, time_limit, path):
@@ -96,26 +118,36 @@ class LogsHandler(object):
 
 class LogsOnFileSystemHandler(LogsHandler):
 
-    def save_log_file(self, name, path, content):
-        if not os.path.exists(path):
-            os.makedirs(path)
-        filename = os.path.join(path, name)
-        app.logger.info("Saving log to {0}".format(filename))
-        with open(filename, "w") as log_file:
-            log_file.write(content)
+    def save_log_file(self, log_files):
+        if type(log_files) is not list:
+            log_files = [log_files]
+        for log in log_files:
+            if not os.path.exists(log.path):
+                os.makedirs(log.path)
+            app.logger.info("Saving log to {0}".format(log.full_path()))
+            with open(log.full_path(), "w") as log_file:
+                log_file.write(log.content)
 
-    def get_log_file(self, path):
-        try:
-            with open(path, "r") as log_file:
-                content = log_file.read()
-                return content
-        except Exception as ex:
-            app.logger.info("Failed to find log file {0}. Error: {1}".format(path, str(ex)))
-            return ""
+    def get_log_file(self, log_files):
+        contents = []
+        if type(log_files) is not list:
+            log_files = [log_files]
+        for log in log_files:
+            try:
+                with open(log.full_path(), "r") as log_file:
+                    content = log_file.read()
+                    contents.append(content)
+            except Exception as ex:
+                app.logger.info("Failed to find log file {0}. Error: {1}".format(log.full_path(), str(ex)))
+                contents.append("")
+        return contents
 
-    def remove_log(self, path):
-        if os.path.exists(path):
-            os.remove(path)
+    def remove_log(self, log_files):
+        if type(log_files) is not list:
+            log_files = [log_files]
+        for log in log_files:
+            if os.path.exists(log.full_path()):
+                os.remove(log.full_path())
 
     def delete_old_logs(self, time_limit, path):
         current_time = time.time()
@@ -144,26 +176,42 @@ class LogsOnExternalMachineHandler(LogsHandler):
         self.ssh = SshService()
         self.env = Environment(loader=PackageLoader("relval", "/templates/bash"))
 
-    def save_log_file(self, name, path, content):
-        self.ssh.connect_to_server();
-        self.ssh.upload_file_to_server(path, name, content)
+    def save_log_file(self, log_files):
+        if type(log_files) is not list:
+            log_files = [log_files]
+        for log in log_files:
+            try:
+                self.ssh.connect_to_server()
+                self.ssh.upload_file_to_server(log.path, log.filename, log.content)
+            except Exception:
+                pass  # do not stop if one file fails
 
-    def get_log_file(self, path):
-        self.ssh.connect_to_server();
-        try:
-            content = self.ssh.get_file_content(path)
-            return content
-        except Exception as ex:
-            app.logger.info("Failed to find log file {0}. Error: {1}".format(path, str(ex)))
-            return ""
-
-    def remove_log(self, path):
+    def get_log_file(self, log_files):
+        if type(log_files) is not list:
+            log_files = [log_files]
+        contents = []
         self.ssh.connect_to_server()
-        try:
-            self.ssh.remove_file(path)
-        except Exception as ex:
-            app.logger.info("Cannot find file {0}".format(path))
-            pass  # file not exists
+        sftp = self.ssh.get_sftp_client()
+        for log in log_files:
+            try:
+                with sftp.open(log.full_path, "r") as f:
+                    contents.append(f.read())
+            except Exception as ex:
+                app.logger.info("Failed to find log file {0}. Error: {1}".format(log.full_path(), str(ex)))
+                return contents.append("")
+        return contents
+
+    def remove_log(self, log_files):
+        if type(log_files) is not list:
+            log_files = [log_files]
+        self.ssh.connect_to_server()
+        sftp = self.ssh.get_sftp_client()
+        for log in log_files:
+            try:
+                sftp.remove(log.full_path)
+            except Exception as ex:
+                app.logger.info("Cannot find file {0}".format(log.full_path))
+                pass  # file not exists
 
     def delete_old_logs(self, time_limit, path):  # time limit in days
         template = self.env.get_template('delete_old_logs.sh')
